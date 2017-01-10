1b45166af3661503906fda02e62ca5ad1f590bdc
mitchtabian <mitch@tabian.ca>
2017-01-09 20:02:44 -0800

    Source Code

--- Bluetooth-Communication/app/build/generated/source/r/debug/com/example/user/bluetooth_communication/R.java
+++ Bluetooth-Communication/app/build/generated/source/r/debug/com/example/user/bluetooth_communication/R.java
@@ -1778,7 +1778,7 @@ containing a value of this type.
         public static final int notification_template_icon_bg=0x7f020053;
     }
     public static final class id {
-        public static final int action0=0x7f0b005e;
+        public static final int action0=0x7f0b005f;
         public static final int action_bar=0x7f0b0045;
         public static final int action_bar_activity_content=0x7f0b0000;
         public static final int action_bar_container=0x7f0b0044;
@@ -1787,7 +1787,7 @@ containing a value of this type.
         public static final int action_bar_subtitle=0x7f0b0025;
         public static final int action_bar_title=0x7f0b0024;
         public static final int action_context_bar=0x7f0b0046;
-        public static final int action_divider=0x7f0b0062;
+        public static final int action_divider=0x7f0b0063;
         public static final int action_menu_divider=0x7f0b0002;
         public static final int action_menu_presenter=0x7f0b0003;
         public static final int action_mode_bar=0x7f0b0042;
@@ -1806,9 +1806,9 @@ containing a value of this type.
         public static final int btnSend=0x7f0b005b;
         public static final int btnStartConnection=0x7f0b0058;
         public static final int buttonPanel=0x7f0b002e;
-        public static final int cancel_action=0x7f0b005f;
+        public static final int cancel_action=0x7f0b0060;
         public static final int checkbox=0x7f0b003c;
-        public static final int chronometer=0x7f0b0065;
+        public static final int chronometer=0x7f0b0066;
         public static final int collapseActionView=0x7f0b001e;
         public static final int contentPanel=0x7f0b0034;
         public static final int custom=0x7f0b003a;
@@ -1819,7 +1819,7 @@ containing a value of this type.
         public static final int editText=0x7f0b005a;
         public static final int edit_query=0x7f0b0047;
         public static final int end=0x7f0b001b;
-        public static final int end_padder=0x7f0b006a;
+        public static final int end_padder=0x7f0b006b;
         public static final int expand_activities_button=0x7f0b0028;
         public static final int expanded_menu=0x7f0b003b;
         public static final int home=0x7f0b0004;
@@ -1827,13 +1827,14 @@ containing a value of this type.
         public static final int icon=0x7f0b002c;
         public static final int ifRoom=0x7f0b001f;
         public static final int image=0x7f0b0029;
-        public static final int info=0x7f0b0069;
-        public static final int line1=0x7f0b0063;
-        public static final int line3=0x7f0b0067;
+        public static final int incomingMessage=0x7f0b005c;
+        public static final int info=0x7f0b006a;
+        public static final int line1=0x7f0b0064;
+        public static final int line3=0x7f0b0068;
         public static final int listMode=0x7f0b0009;
         public static final int list_item=0x7f0b002b;
         public static final int lvNewDevices=0x7f0b0059;
-        public static final int media_actions=0x7f0b0061;
+        public static final int media_actions=0x7f0b0062;
         public static final int middle=0x7f0b001c;
         public static final int multiply=0x7f0b0014;
         public static final int never=0x7f0b0020;
@@ -1867,20 +1868,20 @@ containing a value of this type.
         public static final int src_atop=0x7f0b0016;
         public static final int src_in=0x7f0b0017;
         public static final int src_over=0x7f0b0018;
-        public static final int status_bar_latest_event_content=0x7f0b0060;
+        public static final int status_bar_latest_event_content=0x7f0b0061;
         public static final int submenuarrow=0x7f0b003f;
         public static final int submit_area=0x7f0b0050;
         public static final int tabMode=0x7f0b000b;
-        public static final int text=0x7f0b0068;
-        public static final int text2=0x7f0b0066;
+        public static final int text=0x7f0b0069;
+        public static final int text2=0x7f0b0067;
         public static final int textSpacerNoButtons=0x7f0b0037;
-        public static final int time=0x7f0b0064;
+        public static final int time=0x7f0b0065;
         public static final int title=0x7f0b002d;
         public static final int title_template=0x7f0b0032;
         public static final int top=0x7f0b0023;
         public static final int topPanel=0x7f0b0031;
-        public static final int tvDeviceAddress=0x7f0b005d;
-        public static final int tvDeviceName=0x7f0b005c;
+        public static final int tvDeviceAddress=0x7f0b005e;
+        public static final int tvDeviceName=0x7f0b005d;
         public static final int up=0x7f0b0008;
         public static final int useLogo=0x7f0b0012;
         public static final int withText=0x7f0b0021;
